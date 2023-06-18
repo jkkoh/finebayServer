ee5219d5cde95c8180b2e7ce0cb319e8c29d4e0b
jkkoh <jeongkyu7@gmail.com>
2023-06-19 07:10:58 +0900

    fixing cors issue 2

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -5,6 +5,7 @@ const origins = [
   'http://127.0.0.1:5173',
   'http://www.finebay.co.kr',
   'https://www.finebay.co.kr',
+  'https://main.d1bsvu9izsmlu0.amplifyapp.com',
 ]
 
 module.exports = origins
